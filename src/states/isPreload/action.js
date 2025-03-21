4499cf37383b12d134531e07669d7a859695992e
Snawlz Zr <farizfahrian53@gmail.com>
2025-03-21 23:58:13 +0800

    add action and reducer for authUser and isPreload

--- src/states/isPreload/action.js
+++ src/states/isPreload/action.js
@@ -1,3 +1,38 @@
 /**
  * @TODO: Define all the actions (creator) for the isPreLoad state
  */
+
+import api from '../../api';
+import { setAuthUserActionCreator } from '../authUser/action';
+
+const ActionType = {
+  SET_IS_PRELOAD: 'SET_IS_PRELOAD'
+};
+
+function setIsPreloadActionCreator(isPreload) {
+  return {
+    type: ActionType.SET_IS_PRELOAD,
+    payload: {
+      isPreload,
+    },
+  };
+}
+
+function asyncPreloadProcess() {
+  return async (dispatch) => {
+    try {
+      // preload process
+      const authUser = await api.getOwnProfile();
+      dispatch(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      // fallback process
+      dispatch(setIsPreloadActionCreator(null));
+      console.error(error);
+    } finally {
+      // end preload process
+      dispatch(setIsPreloadActionCreator(false));
+    }
+  };
+}
+
+export { setIsPreloadActionCreator, asyncPreloadProcess, ActionType };
